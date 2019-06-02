963182bcdbc9dcbe437371ed303a6d9112871e2e
Benjamin Liden <benjamin.c.liden@gmail.com>
2019-06-02 16:02:35 -0400

    get port from process.env if exists

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // app.use(express.static(path.resolve(__dirname + "/public")));
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
